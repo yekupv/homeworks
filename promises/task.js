00bf7c7a7b3471a3dd926259a658d80f1acca08e
Makhambet <krutchdd@gmail.com>
2022-02-22 16:34:33 +0600

    task 3

--- promises/task.js
+++ promises/task.js
@@ -1,2 +1,2 @@
 const result = promiseFunction()
-console.log(`Промис ${typeof result === 'string' ? '' : 'НЕ'} работает ${typeof result === 'string' ? result : ''}`)
+result.then((result) => {console.log(`Промис ${typeof result === 'string' ? '' : 'НЕ'} работает ${typeof result === 'string' ? result : ''}`)})
