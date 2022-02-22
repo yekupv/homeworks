4146cd941f71c394a11764772e77d835de22fdd6
Makhambet <krutchdd@gmail.com>
2022-02-22 15:51:33 +0600

    first 2 problems

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,23 @@
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+  arr = [];
+  const descriminant = b**2-4*a*c;
+  if( descriminant < 0) {
+    return arr;
+  }
+  
+  if (descriminant === 0) {
+    const root = -b/(2*a);
+    arr.push(root);
+  }
+  if (descriminant > 0){
+    const root1 = (-b + Math.sqrt(descriminant) )/(2*a);
+    const root2 = (-b - Math.sqrt(descriminant) )/(2*a);
+    arr.push(root1, root2);
+  }
+      
+  
   return arr; // array
 }
 
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,9 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
+  if(arr1.length != arr2.length) return false 
+  let index = 0
+	result = arr1.every((currentValue) => currentValue === arr2[index++])
 
   return result; // boolean
 }
@@ -10,6 +13,8 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+  const filteredArray = arr.filter(currentValue => currentValue > 0 && currentValue % 3 == 0 );
+  resultArr = filteredArray.map((currentValue) => currentValue * 10)
 
   return resultArr; // array
 }
